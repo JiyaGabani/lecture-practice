703296152a0f15dfd1848f6888385a777a00c33c
JiyaGabani <jiya.t2106@gmail.com>
2026-05-14 09:58:19 +0530

    Add files via upload

---
+++ lecture..practice.py
@@ -0,0 +1,49 @@
+Python 3.14.5 (v3.14.5:5607950ef23, May 10 2026, 07:38:09) [Clang 21.0.0 (clang-2100.0.123.102)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> print ("hello,my name is jiya")
+hello,my name is jiya
+>>> # add two number
+>>> a = 10
+>>> b = 10
+>>> sum = a+b
+>>> print (sum)
+20
+>>> 3 sub two number
+SyntaxError: invalid syntax
+>>> # sub two number
+>>> a = 10
+>>> b = 10
+>>> sub = a-b
+>>> print (sub)
+0
+>>> # input()
+>>> name = input("what is your name?")
+what is your name?jiya
+>>> print ("welcome to python programming",name)
+welcome to python programming jiya
+>>> name = "jiya"
+>>> print (type(name))
+<class 'str'>
+>>> # simple calculator
+>>> a = int(input("enter first number:"))
+enter first number:25
+>>> b = int(input("enter second number:"))
+enter second number:30
+>>> print ("addition:", a + b)
+addition: 55
+>>> print ("substraction:", a- b)
+substraction: -5
+>>> print ("multiply:", a * b)
+multiply: 750
+>>> print ("devision:", a / b)
+devision: 0.8333333333333334
+>>> # silly sandwich maker
+>>> bread = input("choose your bread (white/brown):")
+choose your bread (white/brown):white
+>>> filling = input("choose your filling (cheese/jrlly/banana);")
+choose your filling (cheese/jrlly/banana);jrlly
+>>> print ("here is silly sandwich:")# sub two number
+here is silly sandwich:
+    
+
+    
