5faf8dee613649231fa01155b0d46f5f4e6dbeb2
JiyaGabani <jiya.t2106@gmail.com>
2026-05-19 23:24:59 +0530

    Add files via upload

---
+++ 18-05.py
@@ -0,0 +1,59 @@
+Python 3.14.5 (v3.14.5:5607950ef23, May 10 2026, 07:38:09) [Clang 21.0.0 (clang-2100.0.123.102)] on darwin
+Enter "help" below or click "Help" above for more information.
+#Arithmetic operator
+x=2
+y=5
+print(x+y)
+7
+print(x-y)
+-3
+>>> print(x*y)
+10
+>>> print(x/y)
+0.4
+>>> print(x**y)
+32
+>>> print(x//y)
+0
+>>> #Comparison operator
+>>> x=2
+>>> y=5
+>>> print(x==y)
+False
+>>> print(x!=y)
+True
+>>> print(x<y)
+True
+>>> print(x>y)
+False
+>>> print(x<=y)
+True
+>>> print(x>=y)
+False
+>>> #Assignment opreator
+>>> x=2
+>>> y=5
+>>> x=y
+>>> print(x)
+5
+>>> print(y)
+5
+>>> x+=y
+>>> print(x)
+10
+>>> x-=y
+>>> print(x)
+5
+>>> x*=y
+>>> print(x)
+25
+>>> y/=x
+>>> print(y)
+0.2
+>>> x**=y
+>>> print(x)
+1.9036539387158786
+>>> x//=y
+>>> print(x)
+9.0
+>>> #Logical opreator
